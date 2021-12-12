c7fbf956af186d4ee4ef126d02a38a9fcc3cb7ff
SpencerMiske <95696502+SpencerMiske@users.noreply.github.com>
2021-12-11 20:36:46 -0500

    New files

---
+++ PortalGame/core/assets/Map1/notUsing/enemy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="enemy" tilewidth="16" tileheight="16" tilecount="8" columns="4">
+ <image source="Downloads/Treasure Hunters/Treasure Hunters/The Crusty Crew/Sprites/Crabby/02-Run/Run 01.png" width="72" height="32"/>
+</tileset>
---
+++ PortalGame/core/assets/Map1/notUsing/witch.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="witch" tilewidth="16" tileheight="16" tilecount="36" columns="2">
+ <image source="Downloads/Blue Witch/Blue_witch/B_witch_idle.png" width="32" height="288"/>
+</tileset>
