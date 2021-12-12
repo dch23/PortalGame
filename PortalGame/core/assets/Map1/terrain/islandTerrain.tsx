87ce9fd184050d0f20161528a400014ec30c9944
SpencerMiske <95696502+SpencerMiske@users.noreply.github.com>
2021-12-11 20:48:22 -0500

    NEW MAP (NOT CLICKBAIT)

--- PortalGame/core/assets/Map1/terrain/islandTerrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="islandTerrain" tilewidth="16" tileheight="16" tilecount="340" columns="34">
- <image source="Downloads/Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Terrain/Terrain (32x32).png" width="544" height="160"/>
-</tileset>
