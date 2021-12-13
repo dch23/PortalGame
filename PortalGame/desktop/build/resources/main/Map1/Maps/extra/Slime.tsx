d6f849a50618fd3bb7ad2dbf32e12a85b1604a23
dch23 <dcvweb123@gmail.com>
2021-12-12 19:23:59 -0500

    fix paths

---
+++ PortalGame/desktop/build/resources/main/Map1/Maps/extra/Slime.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Slime" tilewidth="16" tileheight="16" tilecount="5" columns="5">
+ <image source="Downloads/platform_metroidvania asset pack v1.01/platform_metroidvania asset pack v1.01/enemies sprites/slime/slime_idle_anim_strip_5.png" width="80" height="16"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ PortalGame/desktop/build/resources/main/Map1/Maps/extra/terrain2.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrain2" tilewidth="32" tileheight="32" tilecount="247" columns="19">
+ <image source="Downloads/Kings and Pigs (1)/Sprites/14-TileSets/Terrain (32x32).png" width="608" height="416"/>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.00994318" y="0.0284091" width="32.081" height="32.0625"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0078125" y="0.0118056" width="32.0221" height="31.7382"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.77636e-15" y="-0.020202" width="32.0202" height="32.0202"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0790514" y="-0.0355731" width="32.0791" height="31.9447"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.05" y="0.025" width="31.95" height="31.975"/>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="0.03125" width="31.7444" height="31.9688"/>
+  </objectgroup>
+ </tile>
+ <tile id="43">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.00555556" y="0" width="31.9944" height="31.9844"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0525253" y="0.0040404" width="31.7657" height="31.9879"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0625" y="0" width="31.9453" height="31.9844"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="0.00277778" width="31.8861" height="31.9972"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.00277778" width="31.9844" height="31.9816"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0078125" y="0.00555556" width="31.9609" height="31.9944"/>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.00887784" width="31.9815" height="32.0217"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0909091" y="0.0484848" width="31.9182" height="31.9504"/>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.125" width="32.0139" height="31.875"/>
+  </objectgroup>
+ </tile>
+</tileset>
